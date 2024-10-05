d4d31d366adacfe835bae6619db53bb82c9148b0
Alinoureddine1 <ali-nd2001@outlook.com>
2024-10-04 20:17:47 -0400

    attempting to fix github actions

--- setup.py
+++ setup.py
@@ -17,25 +17,13 @@ class PostInstallCommand(install):
         install.run(self)
         download_nltk_data()
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name="docugen",
     version="0.1.0",
     author="AliNoureddine1",
     author_email="docugen@example.com",
     description="A tool to generate files from Wikipedia articles",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Alinoureddine1/DocuGen",
     packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
     install_requires=[
         "nltk",
         "wikipedia",
@@ -45,19 +33,13 @@ setup(
         "names",
         "beautifulsoup4",
     ],
-    extras_require={
-        "dev": [
-            "pytest",
-            "flake8",
-        ],
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
     },
     entry_points={
         "console_scripts": [
             "docugen=docugen.docugen:main",
         ],
-    },
-    cmdclass={
-        'develop': PostDevelopCommand,
-        'install': PostInstallCommand,
     },
 )
