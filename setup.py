7ebb0640aaa293015f2ca3b02f2d3dce9932d16c
Alinoureddine1 <ali-nd2001@outlook.com>
2024-10-04 20:09:31 -0400

    fixing nltk data

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 from setuptools import setup, find_packages
+import subprocess
+import sys
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+def download_nltk_data():
+    subprocess.check_call([sys.executable, "-m", "nltk.downloader", "punkt", "averaged_perceptron_tagger", "wordnet"])
+
+class PostDevelopCommand(develop):
+    def run(self):
+        develop.run(self)
+        download_nltk_data()
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+        download_nltk_data()
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -8,7 +25,7 @@ setup(
     version="0.1.0",
     author="AliNoureddine1",
     author_email="docugen@example.com",
-    description="A tool to files from Wikipedia articles",
+    description="A tool to generate files from Wikipedia articles",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Alinoureddine1/DocuGen",
@@ -26,6 +43,7 @@ setup(
         "reportlab",
         "tqdm",
         "names",
+        "beautifulsoup4",
     ],
     extras_require={
         "dev": [
@@ -37,5 +55,9 @@ setup(
         "console_scripts": [
             "docugen=docugen.docugen:main",
         ],
+    },
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
     },
 )
